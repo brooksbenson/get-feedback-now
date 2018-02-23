b54de26a51820cf44e53db4cd5ee9debcdb5c86c
Brooks Benson <brooksbenson01@gmail.com>
2018-02-23 16:51:30 -0700

    .

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,9 +1,9 @@
+console.log(process.env.MONGO_URI);
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  googleCallbackURI: 'https://secure-shore-57185.herokuapp.com',
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
